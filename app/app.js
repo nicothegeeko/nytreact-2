3a5169d91410175988539089c57aa04d94744b29
Nico Flicker <flicker.nico@gmail.com>
2017-06-21 23:39:41 -0700

    working on frontend of application

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 // Include the Main React Dependencies
-var React = require("react");
-var ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
 
-// This code renders a simple "Hello World".
-// ReactDOM takes in two parameters (a single HTML div or element and the HTML target where it will be rendered)
-// The code here will run through webpack and be compiled into plain JavaScript
-// The compiled code will be appended into the index.html file in the id called "app"
-ReactDOM.render(<p>Hello World!</p>, document.getElementById("app"));
+// Include the Main Component
+import Main from "./components/Main";
+
+// This code here allows us to render our main component (in this case "Main")
+ReactDOM.render(<Main />, document.getElementById("app"));
+
+
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// Include the Main React Dependencies
+// import React from "react";
+import ReactDOM from "react-dom";
+
+// Include the Main Component
+import Main from "./Main.js";
+
+// This code here allows us to render our main component (in this case "Main")
+ReactDOM.render(<Main />, document.getElementById("app"));
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Welcome to The Official New York Times Api React Application</h1>
+          <h2>Otherwise Known as: TONYTARA</h2>
+
+        </div>
+        <p className="App-intro">
+          To get started, edit <code> src/App.js </code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
